74d683fbd61086088ed4c555684f22ab6cb90511
hulinhai <hulinhai@derbysoft.com>
2017-07-19 17:09:28 +0800

    新增搜索功能，历史记录等

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,13 +1,150 @@
 <template>
-  <div>
-    search
+  <div class="search">
+    <div class="search-box-wrapper">
+      <search-box ref="searchBox" @query="onQueryChange"></search-box>
+    </div>
+    <div class="shortcut-wrapper" v-show="!query">
+      <div class="shortcut">
+        <div class="hot-key">
+          <h1 class="title">热门搜索</h1>
+          <ul>
+            <li v-for="item in hotKey" class="item" @click="addQuery(item.k)">
+              <span>{{item.k}}</span>
+            </li>
+          </ul>
+        </div>
+        <div class="search-history" v-show="searchHistory.length">
+          <h1 class="title">
+            <span class="text">搜索历史</span>
+            <span class="clear" @click="showConfirm">
+              <i class="icon-clear"></i>
+            </span>
+          </h1>
+          <search-list :searches="searchHistory" @select="addQuery" @delete="deleteSearchHistory"></search-list>
+        </div>
+      </div>
+    </div>
+    <div class="search-result" v-show="query">
+      <suggest :query="query" @listScroll="blurInput" @select="saveSearch"></suggest>
+    </div>
+    <div>
+
+    </div>
+    <confirm ref="confrim"></confirm>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  import SearchBox from 'base/search-box/search-box.vue'
+  import Suggest from 'components/suggest/suggest.vue'
+  import SearchList from 'base/search-list/search-list.vue'
+  import Confrim from 'base/confirm/confirm.vue'
+  import {getHotKey} from 'api/search'
+  import {ERR_OK} from 'api/config'
+  import {mapActions, mapGetters} from 'vuex'
+  export default {
+    data() {
+      return {
+        hotKey: [],
+        query: ''
+      }
+    },
+    created() {
+      this._getHotKey()
+    },
+    computed: {
+      ...mapGetters([
+        'searchHistory'
+      ])
+    },
+    methods: {
+      addQuery(query) {
+        this.$refs.searchBox.setQuery(query)
+      },
+      onQueryChange(query) {
+        this.query = query
+      },
+      blurInput() {
+        this.$refs.searchBox.blur()
+      },
+      saveSearch() {
+        this.saveSearchHistory(this.query)
+      },
+      showConfirm() {
+        console.log(this.$refs.confirm)
+        this.$refs.confrim.show()
+      },
+      _getHotKey() {
+        getHotKey().then((res) => {
+          if (res.code === ERR_OK) {
+            this.hotKey = res.data.hotkey.slice(0, 10)
+          }
+        })
+      },
+      ...mapActions([
+        'saveSearchHistory',
+        'deleteSearchHistory',
+        'clearSearchHistory'
+      ])
+    },
+    components: {
+      Confrim,
+      SearchBox,
+      SearchList,
+      Suggest
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
 
+  .search
+    .search-box-wrapper
+      margin: 20px
+    .shortcut-wrapper
+      position: fixed
+      top: 178px
+      bottom: 0
+      width: 100%
+      .shortcut
+        height: 100%
+        overflow: hidden
+        .hot-key
+          margin: 0 20px 20px 20px
+          .title
+            margin-bottom: 20px
+            font-size: $font-size-medium
+            color: $color-text-l
+          .item
+            display: inline-block
+            padding: 5px 10px
+            margin: 0 20px 10px 0
+            border-radius: 6px
+            background: $color-highlight-background
+            font-size: $font-size-medium
+            color: $color-text-d
+        .search-history
+          position: relative
+          margin: 0 20px
+          .title
+            display: flex
+            align-items: center
+            height: 40px
+            font-size: $font-size-medium
+            color: $color-text-l
+            .text
+              flex: 1
+            .clear
+              extend-click()
+              .icon-clear
+                font-size: $font-size-medium
+                color: $color-text-d
+    .search-result
+      position: fixed
+      width: 100%
+      top: 178px
+      bottom: 0
 </style>
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,8 @@
 import * as types from './mutation-types'
 import {playMode} from 'common/js/config'
 import {shuffle} from 'common/js/util'
+import {saveSearch, deleteSearch, clearSearch} from 'common/js/cache'
+
 export const selectPlay = function({commit, state}, {list, index}) {
   commit(types.SET_SEQUENCELIST, list)
   if (state.mode === playMode.random) {
@@ -23,6 +25,53 @@ export const randomPlay = function ({commit}, {list}) {
   commit(types.SET_CURRENT_INDEX, 0)
   commit(types.SET_FULL_SCREEN, true)
   commit(types.SET_PLAYING_STATE, true)
+}
+
+export const insertSong = function ({commit, state}, song) {
+  /* eslint spaced-comment: ["error", "always"] */
+  let playList = state.playList.slice()
+  let sequenceList = state.sequenceList.slice()
+  let currentIndex = state.currentIndex
+  let currentSong = playList[currentIndex]
+  let fpIndex = findIndex(playList, song)
+  currentIndex++
+  playList.splice(currentIndex, 0, song)
+  if (fpIndex > -1) {
+    if (currentIndex > fpIndex) {
+      playList.splice(fpIndex, 1)
+      currentIndex--
+    } else {
+      playList.splice(fpIndex++, 1)
+    }
+  }
+
+  let currentSindex = findIndex(sequenceList, currentSong) + 1
+  let fsIndex = findIndex(sequenceList, song)
+  sequenceList.splice(currentSindex, 0, song)
+  if (fsIndex > -1) {
+    if (currentSindex > fsIndex) {
+      sequenceList.splice(fsIndex, 1)
+    } else {
+      sequenceList.splice(fsIndex++, 1)
+    }
+  }
+  commit(types.SET_PLAYLIST, playList)
+  commit(types.SET_SEQUENCELIST, sequenceList)
+  commit(types.SET_CURRENT_INDEX, currentIndex)
+  commit(types.SET_FULL_SCREEN, true)
+  commit(types.SET_PLAYING_STATE, true)
+}
+
+export const saveSearchHistory = function ({commit}, query) {
+  commit(types.SET_SEARCH_HISTORY, saveSearch(query))
+}
+
+export const deleteSearchHistory = function ({commit}, query) {
+  commit(types.SET_SEARCH_HISTORY, deleteSearch(query))
+}
+
+export const clearSearchHistory = function ({commit}) {
+  commit(types.SET_SEARCH_HISTORY, clearSearch())
 }
 
 function findIndex(list, song) {
